07b02af1c8a308603a122a5dc02ddb940d373b26
Sebastian Degerman <sebbed89@hotmail.com>
2018-11-22 15:12:34 +0100

    Added favicon.ico Secured urls (https)

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
 	// Get necessary stuff
     function getMatches() {
-        const url = "http://api.unicdn.net/v1/feeds/sportsbook/event/live.jsonp?app_id=ca7871d7&app_key=5371c125b8d99c8f6b5ff9a12de8b85a";
+        const url = "https://api.unicdn.net/v1/feeds/sportsbook/event/live.jsonp?app_id=ca7871d7&app_key=5371c125b8d99c8f6b5ff9a12de8b85a";
         $.ajax({
             url: url,
             cache: true,
